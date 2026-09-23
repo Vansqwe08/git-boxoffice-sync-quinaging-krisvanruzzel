6868b6dd57dcf49ea49067ab205e29dd31042515
Kris Van Ruzzel Quinaging <krisvanquinaging7@gmail.com>
2026-09-23 22:32:40 +0800

    Merge VIP surcharge with group discount and rounding - din.jonvincent

--- test.js
+++ test.js
@@ -14,8 +14,6 @@ assertEqual(tickets.isValidQuantity(4), true, 'a normal order quantity is valid'
 assertEqual(tickets.isValidQuantity(0), false, 'a zero quantity is invalid');
 assertEqual(tickets.isValidQuantity(25), false, 'an order over 20 tickets is invalid');
 
-const price = tickets.calculateTicketPrice(3, 15.5);
-assertEqual(price, 46, 'price for 3 tickets at $15.50 each');
 assertEqual(tickets.calculateTicketPrice(3, 15.5), 47, 'price for 3 tickets at $15.50 each');
 assertEqual(tickets.calculateTicketPrice(5, 20), 90, '10% group discount');
 assertEqual(tickets.calculateTicketPrice(2, 20, true), 60, '50% VIP surcharge');
